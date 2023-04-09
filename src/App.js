f8b170dbfc8ad1b94bfb54d92a033319ba93ad46
Bhupesh Patil <bhupeshp286@gmail.com>
2023-04-09 14:27:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    background: "#f2f2f2",
+  },
+  title: {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    marginBottom: "1rem",
+    textAlign: "center",
+  },
+  description: {
+    fontSize: "1rem",
+    marginBottom: "1rem",
+    textAlign: "center",
+  },
+  button: {
+    fontSize: "1rem",
+    padding: "0.5rem 1rem",
+    border: "none",
+    borderRadius: "0.25rem",
+    background: "#007bff",
+    color: "#fff",
+    cursor: "pointer",
+    transition: "background 0.3s ease",
+    "&:hover": {
+      background: "#0069d9",
+    },
+  },
+  result: {
+    fontSize: "1rem",
+    marginTop: "1rem",
+    textAlign: "center",
+  },
+  number: {
+    fontWeight: "bold",
+  },
+};
 
 function App() {
+  const [randomNumber, setRandomNumber] = useState(null);
+
+  const generateRandomNumber = () => {
+    const newRandomNumber = Math.floor(Math.random() * 100);
+    setRandomNumber(newRandomNumber);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This website uses Git Actions to deploy the application on AWS S3
+    <div style={styles.container}>
+      <h1 style={styles.title}>Random Number Generator</h1>
+      <p style={styles.description}>
+        Click the button to generate a random number between 0 and 100:
+      </p>
+      <button style={styles.button} onClick={generateRandomNumber}>
+        Generate
+      </button>
+      {randomNumber && (
+        <p style={styles.result}>
+          The generated random number is: <span style={styles.number}>{randomNumber}</span>
         </p>
-      </header>
+      )}
     </div>
   );
 }
