a90150b4f9496fd4d5e1b450f2618f953f832ff1
Bhupesh Patil <bhupeshp286@gmail.com>
2023-04-09 18:18:11 -0400

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,71 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-    background: "#f2f2f2",
-  },
-  title: {
-    fontSize: "2rem",
-    fontWeight: "bold",
-    marginBottom: "1rem",
-    textAlign: "center",
-  },
-  description: {
-    fontSize: "1rem",
-    marginBottom: "1rem",
-    textAlign: "center",
-  },
-  button: {
-    fontSize: "1rem",
-    padding: "0.5rem 1rem",
-    border: "none",
-    borderRadius: "0.25rem",
-    background: "#007bff",
-    color: "#fff",
-    cursor: "pointer",
-    transition: "background 0.3s ease",
-    "&:hover": {
-      background: "#0069d9",
-    },
-  },
-  result: {
-    fontSize: "1rem",
-    marginTop: "1rem",
-    textAlign: "center",
-  },
-  number: {
-    fontWeight: "bold",
-  },
-};
+const ClocksContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(180deg, #d2eafc 0%, #c3ddf3 100%);
+`;
+
+const Clock = styled.div`
+  text-align: center;
+  background-color: #2c3e50;
+  border-radius: 50%;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5);
+  padding: 40px;
+  width: 250px;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+
+  @media screen and (max-width: 768px) {
+    width: 200px;
+    height: 200px;
+    padding: 30px;
+  }
+`;
+
+const CityName = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 0 20px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const Time = styled.p`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin: 0;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
 
 function App() {
-  const [randomNumber, setRandomNumber] = useState(null);
+  const [bostonTime, setBostonTime] = useState("");
+  const [seattleTime, setSeattleTime] = useState("");
+  const [mumbaiTime, setMumbaiTime] = useState("");
 
-  const generateRandomNumber = () => {
-    const newRandomNumber = Math.floor(Math.random() * 100);
-    setRandomNumber(newRandomNumber);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const options = {
+        timeZone: "America/New_York",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      };
+      setBostonTime(
+        new Date().toLocaleString("en-US", options)
+      );
+
+      options.timeZone = "America/Los_Angeles";
+      setSeattleTime(
+        new Date().toLocaleString("en-US", options)
+      );
+
+      options.timeZone = "Asia/Kolkata";
+      setMumbaiTime(
+        new Date().toLocaleString("en-US", options) 
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Random Number Generator</h1>
-      <p style={styles.description}>
-        Click the button to generate a random number between 0 and 100:
-      </p>
-      <button style={styles.button} onClick={generateRandomNumber}>
-        Generate
-      </button>
-      {randomNumber && (
-        <p style={styles.result}>
-          The generated random number is: <span style={styles.number}>{randomNumber}</span>
-        </p>
-      )}
-    </div>
+    <ClocksContainer>
+      <Clock>
+        <CityName>Boston</CityName>
+        <Time>{bostonTime}</Time>
+      </Clock>
+      <Clock>
+        <CityName>Seattle</CityName>
+        <Time>{seattleTime}</Time>
+      </Clock>
+      <Clock>
+        <CityName>Mumbai</CityName>
+        <Time>{mumbaiTime}</Time>
+      </Clock>
+    </ClocksContainer>
   );
 }
 
